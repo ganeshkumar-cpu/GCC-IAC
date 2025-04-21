36dd4107b96266cdf1581090a78e50ce1dbfb3b7
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-21 18:59:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,20 @@
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "Public subnets with AZ as keys"
+  type        = map(string)
+}
+
+variable "private_subnets" {
+  description = "Private subnets with AZ as keys"
+  type        = map(string)
+}
 
