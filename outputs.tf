df5fbbfe73b7ac0a86a3b69912e4c3a83b89b865
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-21 18:59:44 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,11 @@
+output "vpc_id" {
+  value = module.grse_vpc.vpc_id
+}
 
+output "public_subnet_ids" {
+  value = module.grse_vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.grse_vpc.private_subnet_ids
+}
