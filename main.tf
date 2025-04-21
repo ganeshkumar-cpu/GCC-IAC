38686762e666b9b02b4824e793299dcd2a8ea822
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-21 18:59:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,16 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
 module "grse_vpc" {
-  source = "./modules/vpc"
-
+  source   = "./modules/vpc"
   vpc_name = "GRSE-vpc"
-  vpc_cidr = "10.0.0.0/16"
+  vpc_cidr = "10.40.0.0/16"
 
   public_subnets = {
-    "ap-south-1a" = "10.0.1.0/24"
-    "ap-south-1b" = "10.0.3.0/24"
+    "ap-south-1a" = "10.40.101.0/24"
+    "ap-south-1b" = "10.40.102.0/24"
   }
 
   private_subnets = {
-    "ap-south-1a" = "10.0.2.0/24"
-    "ap-south-1b" = "10.0.4.0/24"
+    "ap-south-1a" = "10.40.11.0/24"
+    "ap-south-1b" = "10.40.12.0/24"
   }
 }
-
 
