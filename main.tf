05dd59977010f89fb75a78a4d67213bac640f883
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-22 10:30:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,4 +13,17 @@ module "grse_vpc" {
     "ap-south-1b" = "10.40.12.0/24"
   }
 }
+module "grse_security_group" {
+  source = "./modules/security_group"
+  name   = "GRSE-APP&DB-SG"
+}
 
+module "grse_ec2" {
+  source           = "./modules/ec2"
+  instance_name    = "GRSE-APP&DB"
+  instance_type    = "t4g.2xlarge"
+  key_name         = "GRSE-key"
+  root_volume_size = 400
+  ebs_volume_size  = 500
+  security_group_ids = [module.grse_security_group.security_group_id]
+}
