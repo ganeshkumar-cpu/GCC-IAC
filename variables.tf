c83165930723029bcc6973eaeb6e23f8fa2e0987
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-22 11:10:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,72 @@
+# --------------------------
 # VPC variables
-variable "vpc_name" { type = string }
-variable "vpc_cidr" { type = string }
-variable "public_subnets" { type = map(string) }
-variable "private_subnets" { type = map(string) }
+# --------------------------
 
+variable "vpc_name" {
+  type        = string
+  description = "The name of the VPC to be created."
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "The CIDR block for the VPC."
+}
+
+variable "public_subnets" {
+  type        = map(string)
+  description = "A map of public subnet CIDR blocks with AZ names as keys."
+  # Example: { "ap-south-1a" = "10.0.1.0/24", "ap-south-1b" = "10.0.2.0/24" }
+}
+
+variable "private_subnets" {
+  type        = map(string)
+  description = "A map of private subnet CIDR blocks with AZ names as keys."
+  # Example: { "ap-south-1a" = "10.0.11.0/24", "ap-south-1b" = "10.0.12.0/24" }
+}
+
+# --------------------------
 # EC2 variables
-variable "ami" { type = string }
-variable "instance_name" { type = string }
-variable "instance_type" { type = string }
-variable "key_name" { type = string }
-variable "root_volume_size" { type = number }
-variable "ebs_volume_size" { type = number }
+# --------------------------
 
+variable "ami" {
+  type        = string
+  description = "The AMI ID to use for launching the EC2 instance."
+}
+
+variable "instance_name" {
+  type        = string
+  description = "The name to assign to the EC2 instance."
+}
+
+variable "instance_type" {
+  type        = string
+  description = "The type of EC2 instance (e.g., t2.micro, t4g.2xlarge)."
+}
+
+variable "key_name" {
+  type        = string
+  description = "The name of the existing key pair to use for SSH access to the instance."
+}
+
+variable "root_volume_size" {
+  type        = number
+  description = "The size (in GiB) of the root EBS volume attached to the EC2 instance."
+}
+
+variable "ebs_volume_size" {
+  type        = number
+  description = "The size (in GiB) of an additional EBS volume to attach to the EC2 instance."
+}
+
+# --------------------------
 # Security Group variables
-variable "security_group_name" { type = string }
+# --------------------------
+
+variable "security_group_name" {
+  type        = string
+  description = "The name to assign to the security group."
+}
+
 variable "ingress_rules" {
   type = list(object({
     from_port   = number
@@ -21,4 +74,15 @@ variable "ingress_rules" {
     protocol    = string
     cidr_blocks = list(string)
   }))
+  description = "A list of ingress rules defining allowed inbound traffic for the security group."
+  # Example:
+  # [
+  #   {
+  #     from_port   = 22
+  #     to_port     = 22
+  #     protocol    = "tcp"
+  #     cidr_blocks = ["0.0.0.0/0"]
+  #   },
+  #   ...
+  # ]
 }
