9a2625b48e6c188665bd58fe67bce43195a23bf7
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-22 10:52:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,25 @@
 module "grse_vpc" {
-  source   = "./modules/vpc"
-  vpc_name = "GRSE-vpc"
-  vpc_cidr = "10.40.0.0/16"
-
-  public_subnets = {
-    "ap-south-1a" = "10.40.101.0/24"
-    "ap-south-1b" = "10.40.102.0/24"
-  }
-
-  private_subnets = {
-    "ap-south-1a" = "10.40.11.0/24"
-    "ap-south-1b" = "10.40.12.0/24"
-  }
+  source           = "./modules/vpc"
+  vpc_name         = var.vpc_name
+  vpc_cidr         = var.vpc_cidr
+  public_subnets   = var.public_subnets
+  private_subnets  = var.private_subnets
 }
+
 module "grse_security_group" {
-  source = "./modules/security_group"
-  name   = "GRSE-APP&DB-SG"
+  source         = "./modules/security_group"
+  name           = var.security_group_name
+  vpc_id         = module.grse_vpc.vpc_id
+  ingress_rules  = var.ingress_rules
 }
 
 module "grse_ec2" {
-  source           = "./modules/ec2"
-  instance_name    = "GRSE-APP&DB"
-  instance_type    = "t4g.2xlarge"
-  key_name         = "GRSE-key"
-  root_volume_size = 400
-  ebs_volume_size  = 500
+  source             = "./modules/ec2"
+  ami                = var.ami
+  instance_name      = var.instance_name
+  instance_type      = var.instance_type
+  key_name           = var.key_name
+  root_volume_size   = var.root_volume_size
+  ebs_volume_size    = var.ebs_volume_size
   security_group_ids = [module.grse_security_group.security_group_id]
 }
