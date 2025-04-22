d9ba1f9d2737bb2dd414ff9112640935fa4b3b59
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-22 10:38:20 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,1 +1,23 @@
+variable "instance_name" {
+  type = string
+}
 
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "security_group_ids" {
+  type = list(string)
+}
+
+variable "root_volume_size" {
+  type = number
+}
+
+variable "ebs_volume_size" {
+  type = number
+}
