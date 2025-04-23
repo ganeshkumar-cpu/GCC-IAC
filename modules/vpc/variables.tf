27e155887104c01c0178093883fd758ed2379963
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-23 11:58:48 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,15 +1,11 @@
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
 variable "vpc_name" {
-  type = string
-}
-
-variable "vpc_cidr" {
-  type = string
-}
-
-variable "public_subnets" {
-  type = map(string)
-}
-
-variable "private_subnets" {
-  type = map(string)
+  description = "Name of the VPC"
+  type        = string
+  default     = "GRSE-VPC"
 }
