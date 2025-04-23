bca93c518c5eae8ca0b10c91f6a5a8c4557788da
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-23 18:45:56 +0530

    Delete modules/ec2 directory

--- modules/ec2/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "ami" { type = string }
-variable "instance_name" { type = string }
-variable "instance_type" { type = string }
-variable "key_name" { type = string }
-variable "security_group_ids" { type = list(string) }
-variable "root_volume_size" { type = number }
-variable "ebs_volume_size" { type = number }
-
