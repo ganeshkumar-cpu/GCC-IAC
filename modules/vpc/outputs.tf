c88d74078570c265e023d270b817c5bec27fabb5
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-23 12:04:06 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,10 +2,10 @@ output "vpc_id" {
   value = aws_vpc.this.id
 }
 
-output "public_subnet_ids" {
-  value = [for subnet in aws_subnet.public : subnet.id]
+output "public_subnets" {
+  value = [aws_subnet.public_ap_south_1a.id, aws_subnet.public_ap_south_1b.id]
 }
 
-output "private_subnet_ids" {
-  value = [for subnet in aws_subnet.private : subnet.id]
+output "private_subnets" {
+  value = [aws_subnet.private_ap_south_1a.id, aws_subnet.private_ap_south_1b.id]
 }
