e6b9042b08a8b1db98d81359af9791756d1de10c
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-23 18:43:36 +0530

    Update outputs.tf

--- modules/aurora/outputs.tf
+++ modules/aurora/outputs.tf
@@ -1,11 +1,16 @@
-output "db_cluster_endpoint" {
-  value = aws_rds_cluster.this.endpoint
-}
+# modules/aurora/outputs.tf
 
-output "db_writer_endpoint" {
-  value = aws_rds_cluster_instance.writer.endpoint
+output "db_cluster_endpoint" {
+  description = "Endpoint of the Aurora DB cluster"
+  value       = aws_rds_cluster.aurora.endpoint
 }
 
 output "db_reader_endpoint" {
-  value = aws_rds_cluster_instance.reader[0].endpoint
+  description = "Reader endpoint of the Aurora DB cluster"
+  value       = aws_rds_cluster.aurora.reader_endpoint
+}
+
+output "db_sg_id" {
+  description = "Security group ID of the Aurora DB instance"
+  value       = aws_security_group.db_sg.id
 }
