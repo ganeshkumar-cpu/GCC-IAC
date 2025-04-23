c9f68a21562e887870813ed7d5952781cb356599
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-23 13:44:06 +0530

    Update variables.tf

--- modules/asg/variables.tf
+++ modules/asg/variables.tf
@@ -1,60 +1,10 @@
-variable "name" {
-  description = "Name of the Auto Scaling Group"
-  type        = string
-}
-
-variable "ami_id" {
-  description = "AMI ID for the Auto Scaling EC2 instances"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "EC2 instance type for Auto Scaling"
-  type        = string
-}
-
-variable "security_group_ids" {
-  description = "Security group IDs for the EC2 instances"
-  type        = list(string)
-}
-
-variable "user_data" {
-  description = "User data for initializing EC2 instances"
-  type        = string
-}
-
-variable "key_name" {
-  description = "SSH key name for EC2 instances"
-  type        = string
-}
-
-variable "desired_capacity" {
-  description = "Desired capacity of the Auto Scaling Group"
-  type        = number
-}
-
-variable "max_size" {
-  description = "Maximum size of the Auto Scaling Group"
-  type        = number
-}
-
-variable "min_size" {
-  description = "Minimum size of the Auto Scaling Group"
-  type        = number
-}
-
+variable "name" {}
+variable "environment" {}
 variable "subnet_ids" {
-  description = "List of subnet IDs for the Auto Scaling Group"
-  type        = list(string)
+  type = list(string)
 }
-
-variable "target_group_arns" {
-  description = "List of target group ARNs for the ALB"
-  type        = list(string)
-}
-
-variable "root_volume_size" {
-  description = "Root volume size for EC2 instances"
-  type        = number
-  default     = 20
+variable "vpc_id" {}
+variable "instance_type" {}
+variable "desired_capacity" {
+  default = 1
 }
