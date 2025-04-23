a78a1b568cfdc01e5a3963f539e6297e6b8e39f0
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-23 12:04:27 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -3,10 +3,14 @@ resource "aws_lb" "this" {
   internal           = false
   load_balancer_type = "application"
   security_groups   = var.security_group_ids
-  subnets           = var.subnet_ids
-  enable_deletion_protection = false
+  subnets            = var.subnet_ids
 
+  enable_deletion_protection = false
   enable_cross_zone_load_balancing = true
+
+  tags = {
+    Name = var.name
+  }
 }
 
 resource "aws_lb_target_group" "this" {
@@ -14,13 +18,39 @@ resource "aws_lb_target_group" "this" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name = var.target_group_name
+  }
 }
 
-output "dns_name" {
-  value = aws_lb.this.dns_name
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "fixed-response"
+    fixed_response {
+      status_code = 200
+      message_body = "OK"
+    }
+  }
 }
 
 output "target_group_arn" {
   value = aws_lb_target_group.this.arn
+}
+
+output "alb_dns_name" {
+  value = aws_lb.this.dns_name
 }
 
