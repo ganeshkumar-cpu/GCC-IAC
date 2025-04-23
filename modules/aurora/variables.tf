cc082cd52c10981b8d9dc9c91e9b23172c4cb804
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-23 13:45:09 +0530

    Update variables.tf

--- modules/aurora/variables.tf
+++ modules/aurora/variables.tf
@@ -1,46 +1,13 @@
-variable "name" {
-  description = "Name of the Aurora DB Cluster"
-  type        = string
+variable "name" {}
+variable "engine_version" {}
+variable "master_username" {}
+variable "master_password" {}
+variable "instance_class" {}
+variable "writer_az" {}
+variable "reader_az" {
+  default = null
 }
-
-variable "engine_version" {
-  description = "Engine version for the Aurora DB Cluster"
-  type        = string
+variable "subnet_ids" {
+  type = list(string)
 }
-
-variable "master_username" {
-  description = "Master username for Aurora DB Cluster"
-  type        = string
-}
-
-variable "master_password" {
-  description = "Master password for Aurora DB Cluster"
-  type        = string
-  sensitive   = true
-}
-
-variable "db_name" {
-  description = "Database name for the Aurora DB Cluster"
-  type        = string
-}
-
-variable "db_subnet_group" {
-  description = "DB subnet group for Aurora"
-  type        = string
-}
-
-variable "writer_instance_type" {
-  description = "Instance type for the Aurora writer instance"
-  type        = string
-}
-
-variable "reader_instance_type" {
-  description = "Instance type for the Aurora reader instance"
-  type        = string
-}
-
-variable "reader_enabled" {
-  description = "Flag to enable reader instance for Aurora DB Cluster"
-  type        = bool
-  default     = true
-}
+variable "vpc_id" {}
