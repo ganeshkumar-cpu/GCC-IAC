95616b3f24cf09d750de4a4cd3653623ea1bf913
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-23 13:42:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,100 +2,48 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-module "vpc" {
-  source      = "./modules/vpc"
-  cidr_block  = "10.0.0.0/16"
-  vpc_name    = "GRSE-VPC"
+module "prod_instance" {
+  source               = "./modules/ec2_asg"
+  name                 = "prod-server"
+  environment          = "production"
+  subnet_ids           = var.public_subnet_ids
+  vpc_id               = var.vpc_id
+  instance_type        = "m6a.xlarge"
+  desired_capacity     = 1
 }
 
-module "dev_sg" {
-  source = "./modules/security_group"
-  name   = "dev-sg"
+module "dev_instance" {
+  source               = "./modules/ec2_asg"
+  name                 = "dev-server"
+  environment          = "development"
+  subnet_ids           = var.public_subnet_ids
+  vpc_id               = var.vpc_id
+  instance_type        = "t3.micro"
+  desired_capacity     = 1
 }
 
-module "prod_sg" {
-  source = "./modules/security_group"
-  name   = "prod-sg"
-}
-
-module "db_sg" {
-  source = "./modules/security_group"
-  name   = "db-sg"
-}
-
-module "dev_alb" {
-  source             = "./modules/alb"
-  name               = "dev-alb"
-  vpc_id             = module.vpc.vpc_id
-  subnet_ids         = module.vpc.public_subnets
-  security_group_ids = [module.dev_sg.id]
-  target_group_name  = "dev-tg"
-}
-
-module "prod_alb" {
-  source             = "./modules/alb"
-  name               = "prod-alb"
-  vpc_id             = module.vpc.vpc_id
-  subnet_ids         = module.vpc.public_subnets
-  security_group_ids = [module.prod_sg.id]
-  target_group_name  = "prod-tg"
-}
-
-module "dev_asg" {
-  source             = "./modules/asg"
-  name               = "dev-asg"
-  ami_id             = var.dev_ami
-  instance_type      = "t3.micro"
-  key_name           = var.key_name
-  user_data          = file("user_data.sh")
-  desired_capacity   = 1
-  max_size           = 3
-  min_size           = 1
-  subnet_ids         = module.vpc.public_subnets
-  security_group_ids = [module.dev_sg.id]
-  target_group_arns  = [module.dev_alb.target_group_arn]
-}
-
-module "prod_asg" {
-  source             = "./modules/asg"
-  name               = "prod-asg"
-  ami_id             = var.prod_ami
-  instance_type      = "m5.large"
-  key_name           = var.key_name
-  user_data          = file("user_data.sh")
-  desired_capacity   = 2
-  max_size           = 5
-  min_size           = 2
-  subnet_ids         = module.vpc.public_subnets
-  security_group_ids = [module.prod_sg.id]
-  target_group_arns  = [module.prod_alb.target_group_arn]
+module "prod_db" {
+  source               = "./modules/aurora"
+  name                 = "facedetectiontest"
+  engine_version       = "8.0.mysql_aurora.3.04.0"
+  master_username      = "admin"
+  master_password      = var.prod_db_password
+  instance_class       = "db.r5.large"
+  writer_az            = "ap-south-1b"
+  reader_az            = null
+  subnet_ids           = var.db_subnet_ids
+  vpc_id               = var.vpc_id
 }
 
 module "dev_db" {
   source               = "./modules/aurora"
-  name                 = "dev-db"
+  name                 = "gcc-facial-db"
   engine_version       = "8.0.mysql_aurora.3.04.0"
-  db_name              = "devdb"
   master_username      = "admin"
   master_password      = var.dev_db_password
-  writer_instance_type = "db.r5.large"
-  reader_enabled       = true
-  reader_instance_type = "db.r5.large"
+  instance_class       = "db.r6g.xlarge"
   writer_az            = "ap-south-1a"
   reader_az            = "ap-south-1b"
-  db_subnet_group      = module.vpc.db_sub
-
-module "prod_db" {
-  source               = "./modules/aurora"
-  name                 = "prod-db"
-  engine_version       = "8.0.mysql_aurora.3.04.0"
-  db_name              = "proddb"
-  master_username      = "admin"
-  master_password      = var.prod_db_password
-  writer_instance_type = "db.r5.large"
-  reader_enabled       = true
-  reader_instance_type = "db.r5.large"
-  writer_az            = "ap-south-1a"
-  reader_az            = "ap-south-1b"
-  db_subnet_group      = module.vpc.db_subnet_group
+  subnet_ids           = var.db_subnet_ids
+  vpc_id               = var.vpc_id
 }
