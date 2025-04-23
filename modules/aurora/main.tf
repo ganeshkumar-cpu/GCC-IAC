497caea6dc27fe4e1e2a130ab7d84e7848b884be
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-23 12:09:51 +0530

    Update main.tf

--- modules/aurora/main.tf
+++ modules/aurora/main.tf
@@ -1,17 +1,43 @@
 resource "aws_rds_cluster" "this" {
-  cluster_identifier = var.name
-  engine            = "aurora-mysql"
-  master_username   = var.master_username
-  master_password   = var.master_password
-  database_name     = var.db_name
-  engine_version    = var.engine_version
+  cluster_identifier      = var.name
+  engine                 = "aurora-mysql"
+  engine_version         = var.engine_version
+  master_username        = var.master_username
+  master_password        = var.master_password
+  database_name          = var.db_name
+  db_subnet_group_name   = var.db_subnet_group
+  skip_final_snapshot    = true
+  storage_encrypted      = true
+  backup_retention_period = 7
+
+  tags = {
+    Name = var.name
+  }
 }
 
-resource "aws_rds_cluster_instance" "this" {
-  count              = var.reader_enabled ? 2 : 1
-  cluster_identifier = aws_rds_cluster.this.id
-  instance_class     = var.writer_instance_type
-  engine             = "aurora-mysql"
+resource "aws_rds_cluster_instance" "writer" {
+  cluster_identifier      = aws_rds_cluster.this.id
+  instance_class          = var.writer_instance_type
+  engine                  = aws_rds_cluster.this.engine
+  publicly_accessible     = false
+  db_subnet_group_name    = var.db_subnet_group
+
+  tags = {
+    Name = "${var.name}-writer"
+  }
+}
+
+resource "aws_rds_cluster_instance" "reader" {
+  count                  = var.reader_enabled ? 1 : 0
+  cluster_identifier     = aws_rds_cluster.this.id
+  instance_class         = var.reader_instance_type
+  engine                 = aws_rds_cluster.this.engine
+  publicly_accessible    = false
+  db_subnet_group_name   = var.db_subnet_group
+
+  tags = {
+    Name = "${var.name}-reader"
+  }
 }
 
 output "db_cluster_endpoint" {
@@ -19,5 +45,9 @@ output "db_cluster_endpoint" {
 }
 
 output "db_writer_endpoint" {
-  value = aws_rds_cluster.this.reader_endpoint
+  value = aws_rds_cluster_instance.writer.endpoint
+}
+
+output "db_reader_endpoint" {
+  value = aws_rds_cluster_instance.reader[0].endpoint
 }
