4daf14595223203c176ed09d824eaebf2e66b671
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-23 12:13:11 +0530

    Update main.tf

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,8 +1,45 @@
 resource "aws_security_group" "this" {
   name        = var.name
-  description = "Allow inbound traffic for ${var.name}"
+  description = "Security group for ${var.name}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.name
+  }
 }
 
 output "id" {
   value = aws_security_group.this.id
 }
+
+output "name" {
+  value = aws_security_group.this.name
+}
+
