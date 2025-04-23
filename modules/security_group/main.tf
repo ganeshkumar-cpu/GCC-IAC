a7f60735d283a5d483ff35babf7d38239bb258a5
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-23 13:46:35 +0530

    Update main.tf

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,26 +1,16 @@
 resource "aws_security_group" "this" {
   name        = var.name
   description = "Security group for ${var.name}"
+  vpc_id      = var.vpc_id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
   }
 
   egress {
@@ -34,12 +24,3 @@ resource "aws_security_group" "this" {
     Name = var.name
   }
 }
-
-output "id" {
-  value = aws_security_group.this.id
-}
-
-output "name" {
-  value = aws_security_group.this.name
-}
-
