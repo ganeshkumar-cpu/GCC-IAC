babda2120f33868c00ce2006354fa7f1af96f726
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-23 13:46:52 +0530

    Update variables.tf

--- modules/security_group/variables.tf
+++ modules/security_group/variables.tf
@@ -1,4 +1,16 @@
 variable "name" {
-  description = "Name of the Security Group"
-  type        = string
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "ingress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
 }
